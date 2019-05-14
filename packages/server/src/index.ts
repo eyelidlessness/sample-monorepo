19b652014be36f343d669f6a3e3685b1bd779c6f
Roman Yarinovsky <romany@wix.com>
2019-05-14 14:22:32 +0300

    adding express package

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,1 +1,14 @@
 export * from './test-service';
+import express from 'express';
+
+const app = express();
+
+app.get('/', (_req, res) => {
+    res.status(200).send('OK');
+});
+
+app.listen(5000);
+
+export const Func = (test: express.Request) => {
+    return test.param('test');
+};
--- packages/server/test/test.spec.ts
+++ packages/server/test/test.spec.ts
@@ -1,5 +1,6 @@
 import { expect } from 'chai';
 import fs from 'fs';
+import express from 'express';
 
 describe('server', () => {
     it('works', () => {
